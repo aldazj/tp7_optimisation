2ab1929f8ce1be7ba04d5c411638aa9cc5c04dff
aldazj <jayroaldaz@hotmail.com>
2014-12-29 01:02:31 +0100

    dfghj

---
+++ src/Bite.java
@@ -0,0 +1,7 @@
+/**
+ * Created by aldazj on 29.12.14.
+ */
+public class Bite {
+
+
+}
--- src/test/MaxOneFit.java
+++ src/test/MaxOneFit.java
@@ -17,5 +17,6 @@ public class MaxOneFit implements Fitness<Boolean>{
             }
         }
         return falsePositions / individual.size();
+        
     }
 }
